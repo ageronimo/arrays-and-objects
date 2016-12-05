4ecdf8002437d772f94123d383fffc6509a62044
ageronimo <angelageroniimo@gmail.com>
2016-12-05 08:39:21 -1000

    adding them arrays and objects

--- arrays_objects.js
+++ arrays_objects.js
@@ -6,3 +6,23 @@
 
 */
 
+
+// ARRAYS
+// 10 numbers, 5 names, 3 colors
+// SYNTAX YO
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var names = ["Senior Tan", "Zakattack", "Yolo Baggins", "Ohai", "Basic Jeff"];
+var colors = ["light black", "ron burgandy", "highlighter"];
+var adj = ["smart", "special", "yummy", "tiny", "ferocious"];
+var slang = ["scrap", "kobe beef", "da kine", "li 'dat", "pau hana"];
+
+
+// OBJECTS
+var donut = {
+  toppings : ["sprinkles", "chocolate frosting", "o'hare air"],
+  filling : "jelly",
+  shape : "square",
+  count : 12
+};
+
+
